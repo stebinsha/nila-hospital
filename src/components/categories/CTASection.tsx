21646c05b826479c1d762c584160f55fab446aa7
stebinsha <stebisteni@gmail.com>
2026-01-20 13:56:19 +0530

    Update project with responsive <br> and fixes

--- src/components/categories/CTASection.tsx
+++ src/components/categories/CTASection.tsx
@@ -12,14 +12,14 @@ export default function CTASection() {
         {/* Text content */}
         <div className="relative z-10 w-full md:w-2/3  md:text-left">
           <h2 className="text-4xl font-bold mb-6 leading-tight">
-            Healing starts
-            {/* Line break only on small screens */}
-            <br className="md:hidden" /> 
+            Healing starts  <br className="md:hidden" /> 
             here
-            <br className="md:hidden" />
+           <br className="hidden lg:block" />
+            {/* Line break only on small screens */}
+            <br className="md:hidden " />
             we'll walk with you
           </h2>
-
+         
           <button className="bg-gray-900 px-8 py-3 rounded-full font-semibold hover:bg-black transition">
             Take Assessment
           </button>
